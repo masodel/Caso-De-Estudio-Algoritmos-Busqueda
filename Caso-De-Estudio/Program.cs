75853aac2716b37e77901449ef44cff3eee0d608
Mauro Delgado <maurods@uamv.edu.ni>
2025-11-22 13:47:32 -0600

    Add project files.

---
+++ Caso-De-Estudio/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Caso_De_Estudio
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
